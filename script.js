8d6f1c5c954e1bd63bb5bb82b5f13ba50d437e56
thakuroda <engkuroda>
2019-07-12 10:21:18 -0300

    criar comandos em js

---
+++ script.js
@@ -0,0 +1,85 @@
+// Revisão de If
+
+// let nome = prompt('Digite seu nome');
+// let idade = prompt ('Digite sua idade');
+
+// if(idade >= 30){
+//     alert('Seu véio');
+// } 
+// else if(idade >=25){
+//     alert('Tá cehgando lá!')
+// }
+// else{
+//     alert('Novinho');
+// }
+
+// let frase = idade >= 30 'Seu véio' : 'Novinho';
+
+// alert(frase); 
+
+
+// Revisão de laços While e For;
+
+// Exemplo 1:
+
+// let contador = 0;
+
+// while(contador <= 1000){
+//     console.log(contador);
+//     contador++;
+// }
+
+// Exemplo 2:
+
+// let contador = 1;
+
+// do{
+//     console .log(contador);
+//     contador++;
+// }while(contador <= 1000);
+
+
+// Exemplo FOR:
+// for(let i = 99; i > 0; i--){
+//     console.log(i);
+// }
+
+// Exercício contador de 2 em 2 (100 a 2):
+// for (let i = 100; i > 0 ; i -= 2){
+//     console.log(i);
+// }
+
+// Exemplo posição dos nomes 1:
+
+// let nomes = ['Rafael', 'Leonardo', 'Caroline', 'Ana', 'Gabriela', 'Você'];
+
+// for(let i = o; i < 6; i++){
+//     console.log(nomes[i]);
+// }
+
+// Exemplo posição dos nomes 2:
+
+let nomes = ['Rafael', 'Leonardo', 'Caroline', 'Ana', 'Gabriela', 'Você'];
+
+for(let nome of nomes){
+    console.log(nome);
+}
+
+for(let indice in nomes){
+    console.log(indice);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
